f6de6b093cc42f5827833c0c2fca92d8c9422904
Akims <akims.parahins@visma.com>
2023-10-02 22:33:15 +0300

    Remove https stuff

--- Visma.EmployeeSalaryApp/ClientApp/proxy.conf.js
+++ Visma.EmployeeSalaryApp/ClientApp/proxy.conf.js
@@ -1,7 +1,7 @@
 const { env } = require('process');
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:59243';
+const target = env.ASPNETCORE_HTTP_PORT ? `http://localhost:${env.ASPNETCORE_HTTP_PORT}` :
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:5231';
 
 const PROXY_CONFIG = [
   {
